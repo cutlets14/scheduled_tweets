2b4b32d76faae0e25d165f2ed5a0f3bb15c3fb9f
cutlets14 <cutlets14@gmail.com>
2021-11-09 07:52:09 -0500

    Commit remainder of scheduled tweets apps

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # GET root
+  # Alternatively can use - root to: "main#index"
+  get "/", to: "main#index"
+
+  # GET /about
+  get "about", to: "about#index"
+
+end
