0b6f586dfe91c23885cdab155347cc9930b87131
cutlets14 <cutlets14@gmail.com>
2021-11-12 08:07:48 -0500

    Add sign up page and user registration

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
   # GET /about
   get "about-us", to: "about#index", as: :about
 
+  # GET /sign_up
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
 end
